4860e953f84f37bf7ff24c3b40e1aeff1d3a8acf
mayank4code <119710987+mayank4code@users.noreply.github.com>
2023-03-01 05:37:18 +0530

    keeperApp with All functionality working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
-function createNotes (noteItem){
-    return (
-        <Note 
-            key={noteItem.key}
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
             title={noteItem.title}
             content={noteItem.content}
-        />  
-    );
-}
-
-function App(){
-    return (
-        <div>
-        <Header />
-        {notes.map((noteItem) => {
-    return (
-        <Note 
-            key={noteItem.key}
-            title={noteItem.title}
-            content={noteItem.content}
-        />  
-    );
-}
-)}
-        <Footer />
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
     </div>
-    )
+  );
 }
- export default App ;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -34,9 +34,6 @@ ReactDOM.render(<App />, document.getElementById("root"));
 
 
 
-
-
-
 // import React from "react";
 // import ReactDOM from "react-dom";
 // import App from "./components/App";
